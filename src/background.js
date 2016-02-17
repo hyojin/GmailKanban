6b793a9d796f41ee2f0284c4cb69a867f183197b
Hyojin Kwak <hyojintop@gmail.com>
2016-02-18 08:23:45 +0900

    Add function for logging in background, Add listeners for tab moving, removing

--- src/background.js
+++ src/background.js
@@ -1,22 +1,63 @@
+var DEV_MODE = true;
+
+var consoleLog = function(log) {
+    if (DEV_MODE === true) console.log(log);
+}
+
 var AppInfo = AppInfo || {
-    gmailTab: {},
-    kanbanTab: {}
+    kanbanTab: {},
+    kanbanStatus: false,
+    setKanbanTab: function(tab) {
+        this.kanbanStatus = true;
+        this.kanbanTab = tab;
+    },
+    removeKanbanTab: function() {
+        this.kanbanStatus = false;
+        this.kanbanTab = {};
+    },
 };
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     if (changeInfo.status === 'complete' &&
     tab.url.search('https://mail.google.com/mail/') === 0) {
-        AppInfo.gmailTab = tab;
+        consoleLog('Initilize Gmail Kanban');
         bgMessageController.send({cmd: 'initilize'});
+    }
+});
+
+chrome.tabs.onMoved.addListener(function(tabId, moveInfo) {
+    if (AppInfo.kanbanStatus === true && AppInfo.kanbanTab.id === tabId) {
+        consoleLog('Kanban tab was moved');
+        AppInfo.kanbanTab.index = moveInfo.toIndex;
+    }
+});
+
+chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
+    if (AppInfo.kanbanStatus === true && AppInfo.kanbanTab.id === tabId) {
+        consoleLog('Kanban tab was removed');
+        AppInfo.removeKanbanTab();
     }
 });
 
 var BgMessageController = function() {};
 
 BgMessageController.prototype.listen = function(request, sender, sendResponse) {
+    consoleLog(request);
+    consoleLog(sender);
     if(request.cmd === 'addTicketFromGmail') {
-        console.log(request);
-        console.log(AppInfo.gmailTab.url);
+        consoleLog('cmd: addTicketFromGmail');
+        consoleLog(sender.url);
+        localStorage.setItem('addTicketFromGmail', 'value');
+        if (AppInfo.kanbanStatus === false) {
+            chrome.tabs.create({url: 'kanban.html'}, function(tab) {
+                consoleLog(tab);
+                AppInfo.setKanbanTab(tab);
+            });
+        } else {
+            chrome.tabs.highlight({tabs: AppInfo.kanbanTab.index}, function(window) {
+                consoleLog(window);
+            });
+        }
     }
 };
 
