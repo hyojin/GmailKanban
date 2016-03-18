655eb4a0f091fe2c7adc7a64f6bdc6d6f41d2e46
Hyojin Kwak <hyojintop@gmail.com>
2016-03-19 07:23:13 +0900

    Update gulp tasks for packaging

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,58 @@
 var gulp = require('gulp');
-var fs = require("fs");
-var chromeExtension = require("crx");
-var crx = new chromeExtension();
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var rename = require('gulp-rename');
+var streamify = require('gulp-streamify');
+var uglify = require('gulp-uglify');
+var del = require('del');
+var crx = require('gulp-crx-pack');
+var fs = require('fs');
+var zip = require('gulp-vinyl-zip');
+var execSync = require('child_process').execSync;
 
-gulp.task('default', function() {
-    console.log("test");
-    return crx.load('./src')
-    .then(function(){
-        console.log("test");
-        return crx.loadContents();
-    })
-    .then(function(archiveBuffer){
-        console.log(archiveBuffer);
-        fs.writeFile('./dist/GmailKanban.zip', archiveBuffer);
-        return crx.pack(archiveBuffer);
-    })
-    .then(function(crxBuffer){
-        fs.writeFile('./dist/GmailKanban.crx', crxBuffer);
-    });
+gulp.task('clean:work', function() {
+    return del(['work/**/*']);
 });
+
+gulp.task('copy:work', ['clean:work'],function() {
+    return gulp.src('src/**')
+    .pipe(gulp.dest('work'));
+});
+
+gulp.task('uglify', ['copy:work'],function() {
+    return gulp.src('work/*.js')
+    .pipe(streamify(uglify()))
+    .pipe(gulp.dest('work'));
+});
+
+gulp.task('browserify', ['uglify'], function() {
+    browserify("work/kanban/app.js")
+    .bundle()
+    .pipe(source("app.js"))
+    .pipe(streamify(uglify()))
+    .pipe(rename('bundle.js'))
+    .pipe(gulp.dest("work/kanban/"));
+});
+
+gulp.task('crx', ['browserify'],function() {
+    return gulp.src('work')
+    .pipe(crx({
+        privateKey: fs.readFileSync('keys/chrome.pem', 'utf8'),
+        filename: 'chrome.crx'
+    }))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('copy:chrome-key', ['crx'], function() {
+    execSync('cp keys/chrome.pem work/key.pem', function (err, stdout, stderr) {});
+});
+
+gulp.task('packaging:chrome', ['copy:chrome-key'], function() {
+    return gulp.src('work/**/*')
+    .pipe(zip.dest('dist/chrome.zip'));
+});
+
+gulp.task('build', ['packaging:chrome'], function() {
+});
+
+gulp.task('default', ['build']);
